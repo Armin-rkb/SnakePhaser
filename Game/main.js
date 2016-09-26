d78deadec62c98a14f17f0fd1d021114bfd388b1
Armin Karimi Birgani <armin.rkb@gmail.com>
2016-09-26 12:15:26 +0200

    first commit

---
+++ Game/main.js
@@ -0,0 +1,17 @@
+var game =  new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: Preload,
+                                                        create: Create,
+                                                        update: Update});
+//Loading our assets
+function Preload(){
+  
+}
+
+//Setting up our gameworld
+function Create(){
+
+}
+
+//This function is called once each frame(60fps)
+function Update(){
+
+}
